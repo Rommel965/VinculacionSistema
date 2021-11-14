c4c9fa946e629b1315ff7389ead67e105ab6464f
Puch1998 <94205421+Puch1998@users.noreply.github.com>
2021-11-14 14:51:39 -0600

    Carpeta con la cabecera y pie de pagina

---
+++ templates/sidemenu.php
@@ -0,0 +1,31 @@
+    <div id="sidemenu" class="menu-expanded">
+
+        <div id="header">
+            <div id="title"><span> Proyecto </span></div>
+            <div id="menu-btn">
+                <div class="btn-proyecto"></div>
+                <div class="btn-proyecto"></div>
+                <div class="btn-proyecto"></div>
+            </div>
+        </div>
+
+        <!--<div id="profile">
+            <div class="photo"><img src="16363.png" alt="" width="30%"></div>
+            <div class="name"><span>Usuario</span></div>
+        </div>-->
+
+        <div id="menu-items">
+            <div class="item">
+                <a href="#">
+                    <div class="title"><span>Opcion 1</span></div>
+                </a>
+                <a href="#">
+                    <div class="title"><span>Opcion 2</span></div>
+                </a>
+                <a href="#">
+                    <div class="title"><span>Opcion 3</span></div>
+                </a>
+            </div>
+        </div>
+
+    </div>
