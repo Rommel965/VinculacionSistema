62434172e3b574c394d46b46a646234eed496cd5
Rommel965 <5865@itescam.edu.mx>
2021-11-15 12:46:27 -0600

    paginas

---
+++ pagina3.php
@@ -0,0 +1,44 @@
+<?php include "templates/header.php" ?>
+
+    <section>
+        <div class="btn-container">
+            <button class="btn btn-primary" type="button">Guardar</button>
+            <button class="btn btn-primary" type="button">Vacantes Asignadas</button>
+        </div>
+    </section>
+    <section>
+        <div class="input-container">
+            <input type="text"><br><br><input type="text"><br><br>
+            <select name="" id="">
+            <option selected disabled="">Estado</option>
+                <option value=""> Opcion 1 </option>
+                <option value=""> Opcion 2 </option>
+                <option value=""> Opcion 3 </option>
+                <option value=""> Opcion 4 </option>
+                <option value=""> Opcion 5 </option>
+            </select><br><br>
+            <select name="" id="">
+            <option selected disabled="">Ciudad</option>
+                <option value=""> Opcion 1 </option>
+                <option value=""> Opcion 2 </option>
+                <option value=""> Opcion 3 </option>
+                <option value=""> Opcion 4 </option>
+                <option value=""> Opcion 5 </option>
+            </select><br><br>
+            <select name="" id="">
+            <option selected disabled="">Estado</option>
+                <option value=""> Opcion 1 </option>
+                <option value=""> Opcion 2 </option>
+                <option value=""> Opcion 3 </option>
+                <option value=""> Opcion 4 </option>
+                <option value=""> Opcion 5 </option>
+            </select><br><br>
+            <select name="" id="">
+            <option selected disabled="">Ciudad</option>
+                <option value=""> Opcion 1 </option>
+                <option value=""> Opcion 2 </option>
+                <option value=""> Opcion 3 </option>
+                <option value=""> Opcion 4 </option>
+                <option value=""> Opcion 5 </option>
+            </select><br><br>
+        </section>
---
+++ proyectov1.php
@@ -0,0 +1,67 @@
+<?php include "templates/header.php" ?>
+    
+    <section>
+        <div class="main-container">
+            <input type="text"><input type="text">
+            <select name="" id="">
+                <option selected disabled="">Carrera</option>
+                <option value="">Opcion 1</option>
+                <option value="">Opcion 2</option>
+                <option value="">Opcion 3</option>
+            </select>
+            <a href="pagina2.php"><input type="button" value="Nuevo"></a>
+            
+        </div>
+    </section>
+    <section>
+        <div class="table-container">
+        <table class="table table-light table-bordered">
+                <tbody>
+                    <tr>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+<?php include "templates/footer.php" ?>
