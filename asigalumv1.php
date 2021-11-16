cf31680daccf407db9a7a92df185e12ce2a87fcf
Rommel965 <5865@itescam.edu.mx>
2021-11-16 14:58:06 -0600

    asignacion alumnos

---
+++ asigalumv1.php
@@ -0,0 +1,67 @@
+<?php include "templates/header.php" ?>
+    
+    <section>
+        <div class="main-container">
+           
+            <select name="" id="">
+                <option selected disabled="">Lista Alumnos</option>
+                <option value="">Opcion 1</option>
+                <option value="">Opcion 2</option>
+                <option value="">Opcion 3</option>
+            </select>
+            <button class="btn btn-primary" type="button">Buscar</button>
+            
+        </div>
+    </section>
+    <section>
+        <div class="table-container">
+        <table class="table table-light table-bordered">
+                <tbody>
+                    <tr>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+<?php include "templates/footer.php" ?>
---
+++ asigalumv3.php
@@ -0,0 +1,56 @@
+<?php include "templates/header.php" ?>
+    
+    <section>
+        <div class="table-container">
+        <p>PROYECTOS DISPONIBLES PARA ESTE PERIODO</p>
+        <table class="table table-light table-bordered">
+                <tbody>
+                    <tr>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                        <th width="20%" class="text-center"></th>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                        <td width="20%" class="text-center"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <a href="asigalumv1.php"><input type="button" value="Terminar"></a>
+        </div>
+    </section>
+   
+<?php include "templates/footer.php" ?>
